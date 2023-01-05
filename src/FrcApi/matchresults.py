10b235eef86298b3664b7b7409349963e4dd7ffa
png <gamingwithcole7@gmail.com>
2023-01-05 01:35:10 -0500

    Cleaned up event match results

--- src/FrcApi/matchresults.py
+++ src/FrcApi/matchresults.py
@@ -42,31 +42,43 @@ class MatchResults:
         print(url)
         return response.json()
 
-    def EventMatchResults(self, Event: str, MatchLevel: str = None, TeamNumber: int = None, match_number: int = None, Start: int = None, End: int = None, season: int = None):
+    def event_match_results(self, event: str, match_level: str = None,
+                            team_number: int = None, match_number: int = None,
+                            start: int = None, end: int = None,
+                            season: int = None) -> dict:
         """
-        This fucntion returns the general details about a single or multiple matches.
-        MatchLevel: If u want either qual, playoff, or leave blank for all.
-        TeamNumber: If u want the results for a specific team.
-        match_number: If u want the results for a specific match.
-        Start: The start of the matches u want.
-        End: The end of the matches u want.
-        """
-        if match_number or Start or End:
-            if MatchLevel:
-                if match_number and Start or match_number and End:
-                    raise ValueError("You can't specify both match_number and Start or End")
-            else:
-                raise ValueError("MatchLevel is required if match_number, Start, or End is specified")
-        elif match_number and TeamNumber:
-            raise ValueError("You can't specify both match_number and TeamNumber")
+        Returns the general details about a single or multiple matches.
 
-        url = f"https://frc-api.firstinspires.org/v3.0/{self.SEASON if not season else season}/matches/{Event}?tournamentLevel={MatchLevel}&match_number={match_number}&teamNumber={TeamNumber}&start={Start}&end={End}"
-      
-        response = requests.request("GET", url, headers=self.headers, data=self.payload)
-        if match_number:
-            MatchNumPos = response.text.find(f'"match_number":{match_number},')
-            MatchStart = response.text[0:MatchNumPos].rindex('{"isReplay"')
-            MatchEnd = response.text.find("}]},", MatchNumPos,)
-            return response.text[MatchStart:MatchEnd + 4]
-        else:
-           return response.json()
+        MatchLevel: If you want either qual, playoff, or leave blank for all.
+
+        TeamNumber: If you want the results for a specific team.
+
+        match_number: If you want the results for a specific match.
+
+        Start: The start of the matches you want.
+
+        End: The end of the matches you want.
+        """
+
+        if any([match_number, start, end]) and not match_level:
+            raise ValueError("MatchLevel is required if match_number, start, or end is specified")  # noqa: E501
+
+        elif match_number and any([team_number, start, end]): 
+            raise ValueError("You can't specify both match_number and team_number or start or end")  # noqa: E501
+
+        season = season_check(season, self.SEASON)
+
+        url_args = f"&tournamentLevel={match_level}&teamNumber={team_number}&match_number={match_number}&start={start}&end={end}"  # noqa: E501
+        url = f"{BASEURL}{season}/matches/{event}?{url_args}"
+
+        response = requests.request("GET", url, headers=self.headers,
+                                    data=self.payload)
+        # if match_number:
+        #     MatchNumPos = response.text.find(f'"match_number":{match_number},')
+        #     MatchStart = response.text[0:MatchNumPos].rindex('{"isReplay"')
+        #     MatchEnd = response.text.find("}]},", MatchNumPos,)
+        #     return response.text[MatchStart:MatchEnd + 4]
+        # else:
+        #    return response.json()
+
+        return response.json()
